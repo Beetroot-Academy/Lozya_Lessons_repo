bf3c275749f93433bff6745242295ca85953fbda
Victor Lozinskij <victor.lozinskij@outlook.com>
2022-06-16 00:27:52 +0300

    Adding a base structure and 1-st task

---
+++ HW25/1_task/source/script/main.js
@@ -0,0 +1,49 @@
+let playList = [
+    {
+        author: "LED ZEPPELIN",
+        song: "STAIRWAY TO HEAVEN",
+    },
+    {
+        author: "QUEEN",
+        song: "BOHEMIAN RHAPSODY",
+    },
+    {
+        author: "LYNYRD SKYNYRD",
+        song: "FREE BIRD",
+    },
+    {
+        author: "DEEP PURPLE",
+        song: "SMOKE ON THE WATER",
+    },
+    {
+        author: "JIMI HENDRIX",
+        song: "ALL ALONG THE WATCHTOWER",
+    },
+    {
+        author: "AC/DC",
+        song: "BACK IN BLACK",
+    },
+    {
+        author: "QUEEN",
+        song: "WE WILL ROCK YOU",
+    },
+    {
+        author: "METALLICA",
+        song: "ENTER SANDMAN",
+    },
+];
+
+function AddPlayList(list) {
+    const newElement = document.createElement("ul");
+    newElement.classList.add(`playlist`);
+    let search = document.querySelector(`.hero__section`);
+    search.append(newElement);
+    for (let i = 0; i < list.length; i++) {
+        let newLi = document.createElement(`li`);
+        newLi.classList.add(`playlist__item`);
+        newLi.innerHTML = `<span class="playlist__author">${list[i].author}</span> - <span class="playlist__song">${list[i].song.toLowerCase()}</span>`;
+        search = document.querySelector(`ul`);
+        search.append(newLi);
+    }
+}
+AddPlayList(playList);
