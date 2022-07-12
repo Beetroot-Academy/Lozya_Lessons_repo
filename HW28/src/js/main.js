4770508a6c6041523ea3f69036827d697673839a
Victor Lozinskij <victor.lozinskij@outlook.com>
2022-07-12 18:58:20 +0300

    Adding header

---
+++ HW28/src/js/main.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector(".menu");
+const nav = document.querySelector(".nav");
+
+menu.addEventListener("click", () => {
+    menu.classList.toggle("menu--active");
+    nav.classList.toggle("nav--active");
+});
