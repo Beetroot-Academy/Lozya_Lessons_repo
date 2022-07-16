93c28a2dbbc8499e0dc1533dbcd39b135bdff23d
Victor Lozinskij <victor.lozinskij@outlook.com>
2022-07-16 11:32:22 +0300

    Random commit

--- HW28/src/js/main.js
+++ HW28/src/js/main.js
@@ -1,7 +1,26 @@
+// Burger
 const menu = document.querySelector(".menu");
 const nav = document.querySelector(".nav");
 
 menu.addEventListener("click", () => {
     menu.classList.toggle("menu--active");
     nav.classList.toggle("nav--active");
+});
+
+//Splide
+
+document.addEventListener("DOMContentLoaded", function () {
+    let slider1 = new Splide(`#hero`, {
+        type: "loop",
+        perPage: 1,
+        perMove: 1,
+        autoplay: true,
+        interval: 2500,
+        cover: true,
+        fixedHeight: `100vh`,
+        fixedWidth: `100vh`,
+        arrows: false,
+        rewind: true,
+    });
+    slider1.mount();
 });
