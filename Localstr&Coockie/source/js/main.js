654af932eea511aa1632afe96db9857243112e91
Victor Lozinskij <victor.lozinskij@outlook.com>
2022-07-18 22:12:19 +0300

    trying to connect with mySql

--- Localstr&Coockie/source/js/main.js
+++ Localstr&Coockie/source/js/main.js
@@ -1,53 +1,62 @@
-const API = `https://awesome-wallet-app.herokuapp.com/auth/login`;
+var mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+    host: "46.185.114.91",
+    port: "3306",
+    user: "Maykun",
+    database: "vitek",
+    password: "123456789d",
+});
 
 const form = document.querySelector(`.form`);
-let accessToken;
 
 form.addEventListener(`submit`, logIn);
 
 async function logIn(event) {
-  event.preventDefault();
-  const emailValue = document.querySelector(`#email`).value;
-  const passValue = document.querySelector(`#password`).value;
-  let data = await fetch(API, {
-    headers: { "Content-Type": "application/json" },
-    method: `post`,
-    body: JSON.stringify({
-      email: emailValue,
-      password: passValue,
-    }),
-  });
-  if (data.status == 200) {
-    let json = await data.json();
-    accessToken = json.data.accessToken;
-    document.cookie = `Authorization=${accessToken}`;
-    window.location.href = "user.html";
-  }
+    event.preventDefault();
+    const name = document.querySelector(`#name`).value;
+    const age = document.querySelector(`#age`).value;
+    connection.connect(function (err) {
+        if (err) {
+            return console.error("Ошибка: " + err.message);
+        }
+        let sql = `INSERT INTO clients (name, age) VALUES ('${name}', '${age}')`;
+        connection.query(sql, function (err, result, fields) {
+            if (err) throw err;
+            console.log(`success`);
+        });
+        connection.query(
+            "SELECT * FROM clients",
+            function (err, result, fields) {
+                if (err) throw err;
+                console.log(result);
+            }
+        );
+    });
 }
 
 //task_2
 
-let textarea = document.querySelector('#area');
+let textarea = document.querySelector("#area");
 let textareaValue;
 
-textarea.addEventListener('change', function() {
-textareaValue = textarea.value;
-localStorage.setItem('areavalue', textareaValue);
-// console.log('Hi')
-})
+textarea.addEventListener("change", function () {
+    textareaValue = textarea.value;
+    localStorage.setItem("areavalue", textareaValue);
+    // console.log('Hi')
+});
 
 //task_3
 
-let button = document.querySelector('.light__theme');
-let body = document.querySelector('body');
+let button = document.querySelector(".light__theme");
+let body = document.querySelector("body");
 
-button.addEventListener('click', function(){
-body.classList.toggle('dark__theme')
-let bodyClass = body.className;
-localStorage.setItem('theme', bodyClass);
-
-})
-document.addEventListener('DOMContentLoaded', function() {
-    let bodyClass = localStorage.getItem('theme');
+button.addEventListener("click", function () {
+    body.classList.toggle("dark__theme");
+    let bodyClass = body.className;
+    localStorage.setItem("theme", bodyClass);
+});
+document.addEventListener("DOMContentLoaded", function () {
+    let bodyClass = localStorage.getItem("theme");
     body.classList.add(`${bodyClass}`);
-})
+});
