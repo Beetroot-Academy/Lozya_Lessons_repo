ceff8bffdcfa528930752730725c542fff30acf0
Victor <victor.lozinskij>
2022-07-19 17:57:57 +0300

    Almost done hw29 (have troubles with btn)

--- hw29/src/main.js
+++ hw29/src/main.js
@@ -1,18 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import vuetify from './plugins/vuetify'
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
-Vue.use(BootstrapVue)
-Vue.use(BootstrapVueIcons)
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
-Vue.config.productionTip = false
+Vue.use(vuetify);
+Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
-
+  render: (h) => h(App),
+}).$mount("#app");
